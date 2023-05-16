39db4a868d60efb3447c63d6f492d56f68156776
victorias722 <119906318+victorias722@users.noreply.github.com>
2023-05-16 11:06:11 -0400

    Create PaneOrganizer.java

---
+++ PaneOrganizer.java
@@ -0,0 +1,43 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
+public class PaneOrganizer {
+	private BorderPane _root;
+	private Game _game;
+	private VBox _buttonPane;
+	
+	
+	public PaneOrganizer(){
+		_root = new BorderPane();
+		_root.setStyle("-fx-background-color: blue;");
+		_root.setPrefSize(420,780);
+		Pane gamePane = new Pane();
+		_game = new Game(gamePane);
+		_root.setCenter(gamePane);
+		this.setupButton();
+		
+		
+	}
+	public void setupButton(){
+		_buttonPane = new VBox();
+		Button btn = new Button("Quit");
+		_buttonPane.getChildren().add(btn);
+		_root.setBottom(_buttonPane);
+		btn.setOnAction(new ClickHandler());		
+	}
+	
+	public Pane getRoot(){
+		return _root;
+	}
+
+	private class ClickHandler implements EventHandler<ActionEvent>{
+		@Override
+		public void handle(ActionEvent event){
+		System.exit(0);
+		}
+	}
+}
